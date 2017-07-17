6e6823e318f2d703918e15ccf82f07a4837642ee
Perry R Eising <perry.r.eising@gmail.com>
2017-07-17 11:35:25 -0700

    initial commit.

---
+++ src/main/java/App.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import static spark.Spark.*;
+
+public class App {
+    public static void main(String[] args) {
+
+        get("/form", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "form.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/greeting_card", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            String recipient = request.queryParams("recipient");
+            String sender = request.queryParams("sender");
+            model.put("recipient", recipient);
+            model.put("sender", sender);
+            return new ModelAndView(model, "greeting_card.hbs");
+        }, new HandlebarsTemplateEngine());
+
+    }
+}
